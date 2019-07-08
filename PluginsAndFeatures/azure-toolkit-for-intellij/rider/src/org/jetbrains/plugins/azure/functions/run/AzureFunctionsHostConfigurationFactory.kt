2faa6e6729eac37f5a30de50a8d96f345723f921
Maarten Balliauw <maarten@maartenballiauw.be>
2019-07-08 13:00:25 +0200

    Functions - Run individual function while developing #214

--- PluginsAndFeatures/azure-toolkit-for-intellij/rider/src/org/jetbrains/plugins/azure/functions/run/AzureFunctionsHostConfigurationFactory.kt
+++ PluginsAndFeatures/azure-toolkit-for-intellij/rider/src/org/jetbrains/plugins/azure/functions/run/AzureFunctionsHostConfigurationFactory.kt
@@ -63,6 +63,7 @@ class AzureFunctionsHostConfigurationFactory(type: ConfigurationType)
                     true,
                     RunnableProjectKind.None,
                     "",
+                    "",
                     DotNetStartBrowserParameters()
             )
 
